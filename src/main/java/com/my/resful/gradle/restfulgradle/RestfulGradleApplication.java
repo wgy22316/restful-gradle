2289412a16174d8b277aca57a26d021aff2d53c5
wugy <wu.gongyao@puscene.com>
2018-11-12 20:08:37 +0800

    first commit

---
+++ src/main/java/com/my/resful/gradle/restfulgradle/RestfulGradleApplication.java
@@ -0,0 +1,12 @@
+package com.my.resful.gradle.restfulgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulGradleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/my/resful/gradle/restfulgradle/RestfulGradleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.my.resful.gradle.restfulgradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulGradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
